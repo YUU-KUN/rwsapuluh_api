c148528256a5aa3cdb9ab37f6b115df5df90513f
Indra <iwahyu@student.telkomuniversity.ac.id>
2023-08-17 23:36:56 +0700

    [Fix] Now replace citizens after importing excel

--- app/Imports/CitizenImport.php
+++ app/Imports/CitizenImport.php
@@ -3,15 +3,27 @@
 namespace App\Imports;
 
 use App\Models\Citizen;
-use Maatwebsite\Excel\Concerns\ToModel;
+// use Maatwebsite\Excel\Concerns\ToModel;
+
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Imports\HeadingRowFormatter;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
+use Maatwebsite\Excel\Concerns\WithUpserts;
 
 HeadingRowFormatter::default('none');
 
-class CitizenImport implements ToModel, WithHeadingRow, WithMultipleSheets
+class CitizenImport implements ToCollection, WithHeadingRow, WithMultipleSheets, WithUpserts
 {
+    /**
+     * @return string|array
+     */
+    public function uniqueBy()
+    {
+        return 'id';
+    }
+
 
     public function sheets(): array
     {
@@ -25,14 +37,38 @@ class CitizenImport implements ToModel, WithHeadingRow, WithMultipleSheets
     *
     * @return \Illuminate\Database\Eloquent\Model|null
     */
-    public function model(array $row)
+    // public function model(array $row)
+    // {
+    //     if (!isset($row['Nama'])) {
+    //         return null;
+    //     }
+    
+    //     return new Citizen([
+    //         'name' => $row['Nama'],
+    //         'gender' => $row['Jenis Kelamin'],
+    //         'is_head_of_family' => $row['Status KK'] == 'Ya' || $row['Status KK'] == 'ya' ? true : false,
+    //         'rt' => $row['RT'],
+    //         'position' => $row['Jabatan'],
+    //     ]);
+    // }
+
+    public function collection(Collection $rows)
     {
-        return new Citizen([
-            'name' => $row['Nama'],
-            'gender' => $row['Jenis Kelamin'],
-            'is_head_of_family' => $row['Status KK'] == 'Ya' || $row['Status KK'] == 'ya' ? true : false,
-            'rt' => $row['RT'],
-            'position' => $row['Jabatan'],
-        ]);
+        foreach ($rows as $row) 
+        {
+            if (!isset($row['Nama'])) {
+                return null;
+            }
+            Citizen::updateOrCreate(
+                [
+                    'name' => $row['Nama'],
+                    'gender' => $row['Jenis Kelamin'],
+                    'is_head_of_family' => $row['Status KK'] == 'Ya' || $row['Status KK'] == 'ya' ? true : false,
+                    'rt' => $row['RT'],
+                    'position' => $row['Jabatan'],
+                ],
+            );
+        }
     }
+
 }
