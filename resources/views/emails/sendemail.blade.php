04d30499e048e6081a0124ca00e99617d814781f
Indra <iwahyu@student.telkomuniversity.ac.id>
2023-09-20 13:27:03 +0700

    [Feat] Now able to send email

---
+++ resources/views/emails/sendemail.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>coba kirim email</title>
+</head>
+<body>
+    <h3>{{ $data['name'] }}</h3>
+    <h4>{{ $data['body'] }}</h4>
+    <a href={{ $data['link'] }}>Atur ulang kata sandi</a>
+   
+    <p>Terima Kasih</p>
+</body>
+</html>
